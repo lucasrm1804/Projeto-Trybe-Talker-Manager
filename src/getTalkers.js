1cffcd32ebc20fb1ea3cc96376863947bb898db8
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-03-22 11:53:21 -0300

    Fix: Alteraçoes para rodar novamente o avaliador

--- src/getTalkers.js
+++ src/getTalkers.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 
 const talkers = async () => {
   try { 
-    return JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+    return JSON.parse(await fs.readFile('./talker.json'));
   } catch (err) {
     return err;
   }
